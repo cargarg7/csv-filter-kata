e0f0828f6d2b0fe44062303956949869c1412f5b
cargarg7 <cargarg7@gmail.com>
2023-04-27 16:17:51 +0200

    Extract header to describe test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,8 @@
 import { parseCSV } from './index';
 
 describe('ParseCSV', () => {
+	const header = 'Num _factura,Fecha,Bruto,Neto,IVA,IGIC,Concepto,CIF_cliente,NIF_cliente';
+
 	it('Should throw an ERROR if get one line file', () => {
 		const header = undefined;
 		const lines = ['1,02/05/2019,1000,800,20,,ACERLaptop,B76430134,'];
@@ -8,22 +10,20 @@ describe('ParseCSV', () => {
 	});
 
 	it('Should return just header if no lines', () => {
-		const header = 'Num _factura,Fecha,Bruto,Neto,IVA,IGIC,Concepto,CIF_cliente,NIF_cliente';
 		const lines = [];
 		expect(parseCSV(header, lines)).toEqual({
-			header: 'Num _factura,Fecha,Bruto,Neto,IVA,IGIC,Concepto,CIF_cliente,NIF_cliente',
+			header,
 			lines: [],
 		});
 
 		const lines2 = undefined;
 		expect(parseCSV(header, lines2)).toEqual({
-			header: 'Num _factura,Fecha,Bruto,Neto,IVA,IGIC,Concepto,CIF_cliente,NIF_cliente',
+			header,
 			lines: [],
 		});
 	});
 
 	it('Should remove repited lines with same Num _factura', () => {
-		const header = 'Num _factura,Fecha,Bruto,Neto,IVA,IGIC,Concepto,CIF_cliente,NIF_cliente';
 		const lines = [
 			'1,02/05/2019,1000,800,20,,ACERLaptop,B76430134,',
 			'2,03/08/2019,2000,1800,,10,MacBook Pro,,78544372A',
@@ -37,7 +37,6 @@ describe('ParseCSV', () => {
 	});
 
 	it('Should remove line if CIF and NIF exist in same invoice line', () => {
-		const header = 'Num _factura,Fecha,Bruto,Neto,IVA,IGIC,Concepto,CIF_cliente,NIF_cliente';
 		const lines = ['1,02/05/2019,1000,800,20,,ACERLaptop,B76430134,B76430134'];
 		expect(parseCSV(header, lines)).toEqual({
 			header,
@@ -46,7 +45,6 @@ describe('ParseCSV', () => {
 	});
 
 	it('Should remove line if IVA and IGIC exist in same invoice line', () => {
-		const header = 'Num _factura,Fecha,Bruto,Neto,IVA,IGIC,Concepto,CIF_cliente,NIF_cliente';
 		const lines = ['1,02/05/2019,1000,800,20,20,ACERLaptop,B76430134,'];
 		expect(parseCSV(header, lines)).toEqual({
 			header,
@@ -55,7 +53,6 @@ describe('ParseCSV', () => {
 	});
 
 	it('Should remove line if IVA is not number in same invoice line', () => {
-		const header = 'Num _factura,Fecha,Bruto,Neto,IVA,IGIC,Concepto,CIF_cliente,NIF_cliente';
 		const lines = ['1,02/05/2019,1000,800,TWENTTY,,ACERLaptop,B76430134,'];
 		expect(parseCSV(header, lines)).toEqual({
 			header,
@@ -64,7 +61,6 @@ describe('ParseCSV', () => {
 	});
 
 	it('Should remove line if IGIC is not number in same invoice line', () => {
-		const header = 'Num _factura,Fecha,Bruto,Neto,IVA,IGIC,Concepto,CIF_cliente,NIF_cliente';
 		const lines = ['1,02/05/2019,1000,800,,TWENTY,ACERLaptop,B76430134,'];
 		expect(parseCSV(header, lines)).toEqual({
 			header,
@@ -73,7 +69,6 @@ describe('ParseCSV', () => {
 	});
 
 	it('Should remove line if Net balance is miscalculated in same invoice line', () => {
-		const header = 'Num _factura,Fecha,Bruto,Neto,IVA,IGIC,Concepto,CIF_cliente,NIF_cliente';
 		const lines = ['1,02/05/2019,1000,500,20,,ACERLaptop,B76430134,'];
 		expect(parseCSV(header, lines)).toEqual({
 			header,
@@ -82,7 +77,6 @@ describe('ParseCSV', () => {
 	});
 
 	it('Should remove line if Net balance is not a number in same invoice line', () => {
-		const header = 'Num _factura,Fecha,Bruto,Neto,IVA,IGIC,Concepto,CIF_cliente,NIF_cliente';
 		const lines = ['1,02/05/2019,1000,FIVE HUNDRED,20,,ACERLaptop,B76430134,'];
 		expect(parseCSV(header, lines)).toEqual({
 			header,
@@ -91,7 +85,6 @@ describe('ParseCSV', () => {
 	});
 
 	it('Should remove line if Gross balance is not a number in same invoice line', () => {
-		const header = 'Num _factura,Fecha,Bruto,Neto,IVA,IGIC,Concepto,CIF_cliente,NIF_cliente';
 		const lines = ['1,02/05/2019,ONE THOUSAND,500,20,,ACERLaptop,B76430134,'];
 		expect(parseCSV(header, lines)).toEqual({
 			header,
@@ -100,7 +93,6 @@ describe('ParseCSV', () => {
 	});
 
 	it('Should return line if everything is OK', () => {
-		const header = 'Num _factura,Fecha,Bruto,Neto,IVA,IGIC,Concepto,CIF_cliente,NIF_cliente';
 		const lines = ['1,02/05/2019,1000,800,20,,ACERLaptop,B76430134,'];
 		expect(parseCSV(header, lines)).toEqual({
 			header,
