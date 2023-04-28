43baa18d93a5911ce2db70220107f6644d60fd9f
cargarg7 <cargarg7@gmail.com>
2023-04-28 10:59:28 +0200

    Refactor test creator function one line

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,9 +3,9 @@ import { parseCSV } from './index';
 describe('ParseCSV', () => {
 	const header = 'Num _factura,Fecha,Bruto,Neto,IVA,IGIC,Concepto,CIF_cliente,NIF_cliente';
 
-	it('Should throw an ERROR if get one line file', () => {
+	it('Should throw an ERROR if no header', () => {
 		const header = undefined;
-		const lines = ['1,02/05/2019,1000,800,20,,ACERLaptop,B76430134,'];
+		const lines = [withOneInvoiceLineHaving()];
 		expect(() => parseCSV(header, lines)).toThrow('Header must not be empty');
 	});
 
@@ -25,10 +25,10 @@ describe('ParseCSV', () => {
 
 	it('Should remove repited lines with same Num _factura', () => {
 		const lines = [
-			'1,02/05/2019,1000,800,20,,ACERLaptop,B76430134,',
-			'2,03/08/2019,2000,1800,,10,MacBook Pro,,78544372A',
-			'3,03/12/2019,1000,800,20,, LenovoLaptop,,78544372B',
-			'2,03/12/2019,2000,1800,,10, LenovoLaptop,,78544372A',
+			withOneInvoiceLineHaving({ invoiceId: '1' }),
+			withOneInvoiceLineHaving({ invoiceId: '2' }),
+			withOneInvoiceLineHaving({ invoiceId: '3' }),
+			withOneInvoiceLineHaving({ invoiceId: '2' }),
 		];
 		expect(parseCSV(header, lines)).toEqual({
 			header,
@@ -37,7 +37,8 @@ describe('ParseCSV', () => {
 	});
 
 	it('Should remove line if CIF and NIF exist in same invoice line', () => {
-		const lines = ['1,02/05/2019,1000,800,20,,ACERLaptop,B76430134,B76430134'];
+		const lines = [withOneInvoiceLineHaving({ nif: 'B76430134' })];
+
 		expect(parseCSV(header, lines)).toEqual({
 			header,
 			lines: [],
@@ -45,7 +46,7 @@ describe('ParseCSV', () => {
 	});
 
 	it('Should remove line if IVA and IGIC exist in same invoice line', () => {
-		const lines = ['1,02/05/2019,1000,800,20,20,ACERLaptop,B76430134,'];
+		const lines = [withOneInvoiceLineHaving({ igicTax: '20' })];
 		expect(parseCSV(header, lines)).toEqual({
 			header,
 			lines: [],
@@ -53,7 +54,7 @@ describe('ParseCSV', () => {
 	});
 
 	it('Should remove line if IVA is not number in same invoice line', () => {
-		const lines = ['1,02/05/2019,1000,800,TWENTTY,,ACERLaptop,B76430134,'];
+		const lines = [withOneInvoiceLineHaving({ ivaTax: 'TWENTY' })];
 		expect(parseCSV(header, lines)).toEqual({
 			header,
 			lines: [],
@@ -61,7 +62,7 @@ describe('ParseCSV', () => {
 	});
 
 	it('Should remove line if IGIC is not number in same invoice line', () => {
-		const lines = ['1,02/05/2019,1000,800,,TWENTY,ACERLaptop,B76430134,'];
+		const lines = [withOneInvoiceLineHaving({ igicTax: 'TWENTY' })];
 		expect(parseCSV(header, lines)).toEqual({
 			header,
 			lines: [],
@@ -69,7 +70,7 @@ describe('ParseCSV', () => {
 	});
 
 	it('Should remove line if Net balance is miscalculated in same invoice line', () => {
-		const lines = ['1,02/05/2019,1000,500,20,,ACERLaptop,B76430134,'];
+		const lines = [withOneInvoiceLineHaving({ netImport: '500' })];
 		expect(parseCSV(header, lines)).toEqual({
 			header,
 			lines: [],
@@ -77,7 +78,7 @@ describe('ParseCSV', () => {
 	});
 
 	it('Should remove line if Net balance is not a number in same invoice line', () => {
-		const lines = ['1,02/05/2019,1000,FIVE HUNDRED,20,,ACERLaptop,B76430134,'];
+		const lines = [withOneInvoiceLineHaving({ netImport: 'FIVE HUNDRED' })];
 		expect(parseCSV(header, lines)).toEqual({
 			header,
 			lines: [],
@@ -85,7 +86,7 @@ describe('ParseCSV', () => {
 	});
 
 	it('Should remove line if Gross balance is not a number in same invoice line', () => {
-		const lines = ['1,02/05/2019,ONE THOUSAND,500,20,,ACERLaptop,B76430134,'];
+		const lines = [withOneInvoiceLineHaving({ grossImport: 'ONE THOUSAND' })];
 		expect(parseCSV(header, lines)).toEqual({
 			header,
 			lines: [],
@@ -93,10 +94,24 @@ describe('ParseCSV', () => {
 	});
 
 	it('Should return line if everything is OK', () => {
-		const lines = ['1,02/05/2019,1000,800,20,,ACERLaptop,B76430134,'];
+		const lines = [withOneInvoiceLineHaving()];
 		expect(parseCSV(header, lines)).toEqual({
 			header,
 			lines: [lines[0]],
 		});
 	});
+
+	function withOneInvoiceLineHaving({
+		invoiceId = '1',
+		date = '02/05/2019',
+		grossImport = '1000',
+		netImport = '800',
+		ivaTax = '20',
+		igicTax = '',
+		concept = 'ACERLaptop',
+		cif = 'B76430134',
+		nif = '',
+	} = {}): string {
+		return [invoiceId, date, grossImport, netImport, ivaTax, igicTax, concept, cif, nif].join(',');
+	}
 });
